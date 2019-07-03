f5aca83aa5d7bdc4820442730b3e5da7a673690f
nicholashaynes1 <nicholashaynes1@gmail.com>
2019-07-03 16:28:57 -0600

    Added an inner and outler loop for looping through faces and compnents.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,25 +1,27 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Controller
 {
 	
-	private List<String> myList;
+	private List<String> countSmileys;
 	private List<String> possibleEyes;
 	private List<String> possibleNoses;
 	private List<String> possibleMouths;
 	
+	private List<String> tempFaces;
+	
 	public Controller()
 	{
 //		The initial test array
-		myList = new ArrayList<>();
-		myList.add(":)");
-		myList.add(";(");
-		myList.add(";(");
-		myList.add(";}");
-		myList.add(":-D");
+		countSmileys = new ArrayList<>();
+		countSmileys.add("x0D");
+		countSmileys.add(":-(");
+		countSmileys.add(":..-)");
+		countSmileys.add(";~)");
 		
 		
 //		the array for eyes
@@ -32,17 +34,22 @@ public class Controller
 		possibleNoses = new ArrayList<>();
 		possibleNoses.add("-");
 		possibleNoses.add("~");
+		possibleNoses.add("");
 		
 //		The array for mouths
 		possibleMouths = new ArrayList<>();
 		possibleMouths.add(")");
 		possibleMouths.add("D");
+		
+		
+		
+		tempFaces = new ArrayList<>();
 	}
 	
 	public void start()
 	{
 		
-		countSmileys(myList);
+		System.out.println(countSmileys(countSmileys));
 		
 	}
 	
@@ -50,6 +57,22 @@ public class Controller
 	private int countSmileys(List<String> arr)
 	{
 		int returnMe = 0;
+		int smileyCount = 0;
+		
+		
+//		This outer loop goes through the argument passed and adds them to a temporary arrayList for further analysis      
+		for(String smiley: arr)
+		{
+			tempFaces.clear();
+			tempFaces.addAll(Arrays.asList(smiley.split("")));
+			
+//			The inner loop where we loop through faces components.
+			for(String parts: tempFaces)
+			{
+				
+			}
+			
+		}
 		
 		return returnMe;
 	}
