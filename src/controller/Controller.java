c00e9dda681398c94491bad2320dd5583b982c6c
nicholashaynes1 <nicholashaynes1@gmail.com>
2019-07-02 19:38:51 -0600

    Added the list of possible eyes, noses, and mouths.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -7,15 +7,36 @@ public class Controller
 {
 	
 	private List<String> myList;
+	private List<String> possibleEyes;
+	private List<String> possibleNoses;
+	private List<String> possibleMouths;
 	
 	public Controller()
 	{
+//		The initial test array
 		myList = new ArrayList<>();
 		myList.add(":)");
 		myList.add(";(");
 		myList.add(";(");
 		myList.add(";}");
 		myList.add(":-D");
+		
+		
+//		the array for eyes
+		possibleEyes = new ArrayList<>();
+		possibleEyes.add(":");
+		possibleEyes.add(";");
+		
+		
+//		the array for noses
+		possibleNoses = new ArrayList<>();
+		possibleNoses.add("-");
+		possibleNoses.add("~");
+		
+//		The array for mouths
+		possibleMouths = new ArrayList<>();
+		possibleMouths.add(")");
+		possibleMouths.add("D");
 	}
 	
 	public void start()
